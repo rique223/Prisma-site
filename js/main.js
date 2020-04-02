4461f63b7e032caaae69dc26a6d016cab51264f8
rique223 <henrique.jobs1@gmail.com>
2020-04-02 14:13:34 -0300

    Página Nossos-Clientes implementada

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+$('.owl-carousel').owlCarousel({
+    dots: false,
+    nav: true,
+    margin: 10,
+    loop: true,
+    autoWidth: true,
+    items: 1,
+    navText: ["<img src='./img/ico-left.png'>", "<img src='./img/ico-right.png'>"]
+})

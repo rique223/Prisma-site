a1e2902838142a9e804e9f7d9c06fd6eec397a98
rique223 <henrique.jobs1@gmail.com>
2020-04-07 15:03:54 -0300

    Configuração mobile implementada Institucional e Nossos Clientes
    Mobile implementada, os paddings e margins da navbar precisam ser arrumadas, estão causando overflow

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-$('.owl-carousel').owlCarousel({
-    dots: false,
-    nav: true,
-    margin: 10,
-    loop: true,
-    autoWidth: true,
-    items: 1,
-    navText: ["<img src='./img/ico-left.png'>", "<img src='./img/ico-right.png'>"]
-})
+if (window.matchMedia("(min-width:768px)").matches) {
+    $('.owl-carousel').owlCarousel({
+        dots: false,
+        nav: true,
+        margin: 10,
+        loop: true,
+        autoWidth: true,
+        items: 1,
+        navText: ["<img src='./img/ico-left.png'>", "<img src='./img/ico-right.png'>"]
+    })
+} else {
+    $('.owl-carousel').owlCarousel({
+        dots: false,
+        nav: false,
+        margin: 10,
+        loop: true,
+        autoWidth: false,
+        items: 1
+    })
+}
